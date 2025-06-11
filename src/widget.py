2416db6f24d67d2c9d1d1c14b89ef6526caae735
marina <mrnbahova@yandex.ru>
2025-06-11 17:28:56 +0800

    all mask

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> str:
+    parts = info.split()
+    type_info = " ".join(parts[:-1])
+    number = parts[-1]
+    if "счет" in info.lower():
+        mask_number = get_mask_account(number)
+    else:
+        mask_number = get_mask_card_number(number)
+    return f"{type_info} {mask_number}"
+
+def get_date(data: str) -> str:
+
+    pass

25b59f88ca570a798f0c657e15951872f2af6fba
marina <mrnbahova@yandex.ru>
2025-06-11 16:44:21 +0800

    all file

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция скрывает некоторые символы номера карты"""
+    mask_card_number = card_number[0:4] + " " + card_number[5:7] + "** ****" + " " + card_number[-4:]
+    return mask_card_number
+
+
+def get_mask_account(account: str) -> str:
+    """Функция скрывает номер счета и выводит последние цифры"""
+    mask_account = "**" + account[-4:]
+    return mask_account

30657076e5ce885dfe7230fe844dcaebf7d38b99
marina <mrnbahova@yandex.ru>
2025-07-07 23:00:11 +0800

    bug fixes

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,15 @@
-#from main import filter_by_state, sort_by_date
-
 def filter_by_state(DICTIONARIES, state: list) -> list:
-    ''' Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-    state соответствует указанному значению '''
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению"""
     new_dictionaries = []
     for slovar in DICTIONARIES:
         for value in slovar.values():
-            if value == 'EXECUTED':
+            if value == "EXECUTED":
                 new_dictionaries.append(slovar)
     return new_dictionaries
 
 
 def sort_by_date(DICTIONARIES_DATA: list) -> list:
-    ''' Функция возвращает новый список, отсортированный по дате '''
-    sorted_dictionaries_data = sorted(DICTIONARIES_DATA, key=lambda x: x['date'], reverse=True)
-    #print(sorted_dictionaries_data)
+    """Функция возвращает новый список, отсортированный по дате"""
+    sorted_dictionaries_data = sorted(DICTIONARIES_DATA, key=lambda x: x["date"], reverse=True)
     return sorted_dictionaries_data
-

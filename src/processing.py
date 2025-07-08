dc6c4ecc1057a9c05a59e258f694f6bf56311048
marina <mrnbahova@yandex.ru>
2025-07-08 13:29:43 +0800

    fixed variable names to lower case

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,15 @@
-def filter_by_state(DICTIONARIES, state: list) -> list:
+def filter_by_state(dictionaries, state: list) -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению"""
     new_dictionaries = []
-    for dictionary in DICTIONARIES:
+    for dictionary in dictionaries:
         for value in dictionary.values():
             if value == "EXECUTED":
                 new_dictionaries.append(dictionary)
     return new_dictionaries
 
 
-def sort_by_date(DICTIONARIES_DATA: list) -> list:
+def sort_by_date(dictionaries_data: list) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
-    sorted_dictionaries_data = sorted(DICTIONARIES_DATA, key=lambda x: x["date"], reverse=True)
+    sorted_dictionaries_data = sorted(dictionaries_data, key=lambda x: x["date"], reverse=True)
     return sorted_dictionaries_data

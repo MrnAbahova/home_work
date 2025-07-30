89bdbd7e8545b5c3800ddcf710432b2ac56b0f5b
marina <mrnbahova@yandex.ru>
2025-07-30 17:41:09 +0800

    изменение описания проекта и корректировка функции filter_by_state

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,7 @@
-from src.widget import mask_account_card, get_date
 import pytest
+
+from src.widget import mask_account_card, get_date
+
 
 
 @pytest.mark.parametrize(
@@ -12,11 +14,12 @@ import pytest
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
         ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-        ("Счет 73654108430135874305", "Счет **4305")])
-
-def test_mask_accoud_card(card_number: str, expected: str) -> str:
-    """ Тест корректности маскировки карты """
-    assert mask_accoud_card(card_number) == expected
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_account_card(account_card: str, expected: str) -> str:
+    """Тест корректности маскировки карты"""
+    assert mask_account_card(account_card) == expected
 
 
 @pytest.mark.parametrize(

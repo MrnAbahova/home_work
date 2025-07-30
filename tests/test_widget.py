7fdf46801a33ed0562bdb2a9236b83b3482df7a1
marina <mrnbahova@yandex.ru>
2025-07-30 16:09:52 +0800

    создан тест корректности маскировки карты

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -12,9 +12,8 @@ import pytest
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
         ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-    ],
-)
+        ("Счет 73654108430135874305", "Счет **4305")])
+
 def test_mask_accoud_card(card_number: str, expected: str) -> str:
     """ Тест корректности маскировки карты """
     assert mask_accoud_card(card_number) == expected
